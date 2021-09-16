a1b8ace5d060b51d883255ee87dbca2b24ea79c2
huseyinafsin <huseyinafsin4@gmail.com>
2021-09-16 10:32:14 +0300

    Initial commit

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-KRNCUB4\\SQLEXPRESS;Database=CoreBlogDb; Integrated Security=True;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writter> Writters { get; set; }
+    }
+}
---
+++ DataAccessLayer/Repositories/CommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Repositories
+{
+    class CommentRepository : GenericRepository<Comment>
+    {
+       
+
+      
+    }
+}
