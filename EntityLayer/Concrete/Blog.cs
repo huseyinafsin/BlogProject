825eb884e8c4ff70582a4b077ff6e5a95b11a181
huseyinafsin <huseyinafsin4@gmail.com>
2021-09-16 10:40:38 +0300

    Blog removed spaces

--- EntityLayer/Concrete/Blog.cs
+++ EntityLayer/Concrete/Blog.cs
@@ -10,7 +10,6 @@ namespace EntityLayer.Concrete
     public class Blog
     {
         [Key]
-
         public int BlogID { get; set; }
         public string BlogTitle { get; set; }
         public string BlogContent { get; set; }
